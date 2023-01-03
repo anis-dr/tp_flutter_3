05b015b67c9b52986864e43079aef09710d31881
Anis Lazaar <anis00723@gmail.com>
2023-01-03 02:40:17 +0100

    dish liking works

--- lib/items_list_screen.dart
+++ lib/items_list_screen.dart
@@ -4,10 +4,12 @@ import 'constants.dart';
 import 'login_screen.dart';
 
 class Dish {
+  final int id;
   final String name;
   bool fav;
 
   Dish({
+    required this.id,
     required this.name,
     required this.fav,
   });
@@ -23,71 +25,108 @@ class ItemsListScreen extends StatefulWidget {
 enum MenuOptions { LOGOUT }
 
 class _ItemsListScreenState extends State<ItemsListScreen> {
-  final List<Dish> _dishesList = [
-    Dish(name: "Pizza", fav: false),
-    Dish(name: "Pasta", fav: false),
-    Dish(name: "Burger", fav: false),
-    Dish(name: "Salad", fav: false),
-    Dish(name: "Sandwich", fav: false),
-    Dish(name: "Soup", fav: false),
-    Dish(name: "Fries", fav: false),
-    Dish(name: "Tacos", fav: false),
-    Dish(name: "Burrito", fav: false),
-    Dish(name: "Sushi", fav: false),
-    Dish(name: "Ramen", fav: false),
-    Dish(name: "Noodles", fav: false),
-    Dish(name: "Steak", fav: false),
-    Dish(name: "Chicken", fav: false),
-    Dish(name: "Fish", fav: false),
-    Dish(name: "Rice", fav: false),
-    Dish(name: "Eggs", fav: false),
-    Dish(name: "Pancakes", fav: false),
-    Dish(name: "Waffles", fav: false),
-    Dish(name: "Ice Cream", fav: false),
-    Dish(name: "Cake", fav: false),
-    Dish(name: "Pie", fav: false),
-    Dish(name: "Cookies", fav: false),
-    Dish(name: "Donuts", fav: false),
-    Dish(name: "Cupcakes", fav: false),
-    Dish(name: "Muffins", fav: false),
-    Dish(name: "Candy", fav: false),
-    Dish(name: "Chocolate", fav: false),
-    Dish(name: "Cheese", fav: false),
-    Dish(name: "Bread", fav: false),
-    Dish(name: "Peanut Butter", fav: false),
-    Dish(name: "Jelly", fav: false),
-    Dish(name: "Jam", fav: false),
-    Dish(name: "Honey", fav: false),
-    Dish(name: "Milk", fav: false),
-    Dish(name: "Juice", fav: false),
-    Dish(name: "Water", fav: false),
-    Dish(name: "Tea", fav: false),
-    Dish(name: "Coffee", fav: false),
-    Dish(name: "Soda", fav: false),
-    Dish(name: "Beer", fav: false),
-    Dish(name: "Wine", fav: false),
-  ];
+  final List<Dish> _dishesList = [];
+  bool _isAdmin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      getData();
+    });
+  }
+
+  Future<void> onMenuOptionSelected(MenuOptions result) async {
+    switch (result) {
+      case MenuOptions.LOGOUT:
+        await supabase.auth.signOut();
+        // Navigate to the login screen
+        if (!mounted) return;
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const LoginScreen(),
+          ),
+        );
+        break;
+    }
+  }
+
+  Future<void> getData() async {
+    var dishes = await supabase.rpc('get_dishes_for_user', params: {
+      'user_id_input': supabase.auth.currentUser!.id,
+    });
+
+    final isAdmin = await supabase.rpc("is_admin");
+
+    setState(() {
+      _isAdmin = isAdmin;
+      _dishesList.clear();
+      for (var dish in dishes as List) {
+        _dishesList.add(
+          Dish(
+            id: dish['id'] as int,
+            name: dish['name'] as String,
+            fav: dish['liked'] as bool,
+          ),
+        );
+      }
+    });
+  }
+
+  toggleFav(Dish dish) {
+    setState(() {
+      dish.fav = !dish.fav;
+    });
+  }
+
+  Future<void> onLikePressed(int index) async {
+    toggleFav(_dishesList[index]);
+    var dishId = _dishesList[index % _dishesList.length].id;
+    if (_dishesList[index % _dishesList.length].fav) {
+      try {
+        await supabase.from("like").insert([
+          {
+            "dish_id": dishId,
+            "user_id": supabase.auth.currentUser!.id,
+          }
+        ]);
+      } catch (e) {
+        debugPrint(e.toString());
+        toggleFav(_dishesList[index]);
+      }
+    } else {
+      try {
+        await supabase.from("like").delete().match(
+          {
+            "dish_id": dishId,
+            "user_id": supabase.auth.currentUser!.id,
+          },
+        );
+      } catch (e) {
+        debugPrint(e.toString());
+        toggleFav(_dishesList[index]);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: _isAdmin
+          ? FloatingActionButton(
+              onPressed: () {
+                // TODO: Add a new dish
+                Navigator.pushNamed(context, '/add_dish');
+              },
+              child: const Icon(Icons.add),
+            )
+          : null,
       appBar: AppBar(
         actions: <Widget>[
           PopupMenuButton(
             onSelected: (MenuOptions result) async {
-              switch (result) {
-                case MenuOptions.LOGOUT:
-                  await supabase.auth.signOut();
-                  // Navigate to the login screen
-                  if (!mounted) return;
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginScreen(),
-                    ),
-                  );
-                  break;
-              }
+              await onMenuOptionSelected(result);
             },
             icon: const Icon(Icons.more_vert),
             itemBuilder: (BuildContext context) {
@@ -100,7 +139,7 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
             },
           ),
         ],
-        title: const Text('Movies List'),
+        title: const Text('Dishes'),
       ),
       body: SafeArea(
         child: Container(
@@ -122,10 +161,7 @@ class _ItemsListScreenState extends State<ItemsListScreen> {
                           : Colors.grey,
                     ),
                     onPressed: () {
-                      setState(() {
-                        _dishesList[index % _dishesList.length].fav =
-                            !_dishesList[index % _dishesList.length].fav;
-                      });
+                      onLikePressed(index);
                     },
                   ),
                 ),
